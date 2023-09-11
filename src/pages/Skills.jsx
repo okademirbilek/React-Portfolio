5df85f358c7e0092ff3499f53a4c2267e51f94be
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2023-09-12 01:05:36 +0300

    whole

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -72,17 +72,17 @@ export default function Skills() {
   useEffect(() => {
     async function fetchCachedImages() {
       const imageUrls = [
-        "./src/assets/react.svg",
-        "./src/assets/vite.svg",
-        "./src/assets/sass.png",
-        "./src/assets/js.png",
-        "./src/assets/html.png",
-        "./src/assets/css.png",
-        "./src/assets/boot.png",
-        "./src/assets/three.png",
-        "./src/assets/api.png",
-        "./src/assets/firebase.png",
-        "./src/assets/blender.png",
+        "https://okandemirbilek.netlify.app/src/assets/react.svg",
+        "https://okandemirbilek.netlify.app/src/assets/vite.svg",
+        "https://okandemirbilek.netlify.app/src/assets/sass.png",
+        "https://okandemirbilek.netlify.app/src/assets/js.png",
+        "https://okandemirbilek.netlify.app/src/assets/html.png",
+        "https://okandemirbilek.netlify.app/src/assets/css.png",
+        "https://okandemirbilek.netlify.app/src/assets/boot.png",
+        "https://okandemirbilek.netlify.app/src/assets/three.png",
+        "https://okandemirbilek.netlify.app/src/assets/api.png",
+        "https://okandemirbilek.netlify.app/src/assets/firebase.png",
+        "https://okandemirbilek.netlify.app/src/assets/blender.png",
       ];
 
       const cachedImagesArray = [];
