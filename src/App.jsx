2137e2defd21d3fd43a8c5c8b105bf88bf1ef91a
Okan Demirbilek <48480726+okademirbilek@users.noreply.github.com>
2023-09-12 02:03:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -47,21 +47,20 @@ function App() {
   //   { skill: "Firebase", imgSrc: firebase },
   //   { skill: "Blender", imgSrc: blender },
   // ];
-  useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker
-        .register("https://okandemirbilek.netlify.app/serviceWorker.js")
-        .then((registration) => {
-          console.log(
-            "Service Worker registered with scope:",
-            registration.scope
-          );
-        })
-        .catch((error) => {
-          console.error("Service Worker registration failed:", error);
-        });
-    }
-  }, []);
+
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+      .register("https://okandemirbilek.netlify.app/serviceWorker.js")
+      .then((registration) => {
+        console.log(
+          "Service Worker registered with scope:",
+          registration.scope
+        );
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  }
 
   // useEffect(() => {
   //   preloadImages(data);
